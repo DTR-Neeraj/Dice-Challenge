8aa994a1156b4e52eea763b24b1cd67233e5409b
DTR-Neeraj <66261779+DTR-Neeraj@users.noreply.github.com>
2021-09-17 23:31:57 -1200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "Dice" + randomNumber1 + ".png";
+var randomImageSource = "images/" + randomDiceImage;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource2 = "images/Dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩Play 1 Wins";
+}
+else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Play 2 Wins🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw";
+}
